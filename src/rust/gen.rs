98ff803769df2d6afd5c17158b50872a91cdd9f5
Sunil <sunil@live.com>
2025-03-16 18:03:40 -0700

    changed the approach of reading message file when several files share same hash value

--- src/rust/gen.rs
+++ src/rust/gen.rs
@@ -4,6 +4,8 @@ use std::convert::TryInto;
 use std::time::{SystemTime,UNIX_EPOCH};
 use simweb::WebData;
 use std::path::PathBuf;
+use std::fs::OpenOptions;
+use std::io::{Read,SeekFrom,Seek,Write};
 use data;
 use json;
 use std::fs;
@@ -12,6 +14,12 @@ use DATA_DIR;
 
 pub struct GenPage {
     
+}
+
+struct DirEntry {
+    num: u32,
+    time: u64,
+    key: [u8;KEY_LEN],
 }
 
 impl simweb::WebPage for GenPage {
@@ -31,18 +39,103 @@ impl simweb::WebPage for GenPage {
         let key = xx[0..KEY_LEN].try_into().unwrap();
         // skip a dir file for quick test
         let hash = data::calc_hash(key);
-       
+        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
         let mut file_dat = PathBuf::new();
         file_dat.push(DATA_DIR);
         // how to fix:
-        // for every has create hash.col file, the file contains all active keys and actual .dat files for then
+        // for every has create hash.dic file, the file contains all active keys and actual .dat files for then
         // its name usually hash-offs.dat where offs is a number not taken yet
         file_dat.push(format!{"{hash}"});
+        file_dat.set_extension("dic");
+        let mut file = OpenOptions::new().write(true).create(true).read(true).open(&file_dat).map_err(|e| format!{"{e:?}"})?;
+        // try to read the current dir first
+        let mut buffer = [0; 5]; // Buffer to hold 5 bytes
+        let mut buffer2 = [0; 14];
+        let mut buffer3 = [0; KEY_LEN];
+        let mut clashes = Vec::new();
+        let mut max_cla = 0_u32;
+        let mut broken_dir = false;
+        loop {
+            match file.read_exact(&mut buffer) { // read num
+                Ok(_) => {
+                    match ascii_bytes_to_number(&buffer) {
+                        Ok(n) => {
+                            match file.read_exact(&mut buffer2) { // read date of creation
+                                Ok(_) => {
+                                    match ascii_bytes_to_number(&buffer2) {
+                                       Ok(time) => {
+                                           // can be skeeped if time passed
+                                           match file.read_exact(&mut buffer3) {
+                                              Ok(_) => {
+                                                  if now.as_millis()- (time as u128) < 1000_u128*60*60*24*7  { // 7 days
+                                                        // TODO check if key == buffer3 (sanity check)
+                                                        let entry = DirEntry{num:n as u32, time: time as u64, key: buffer3.clone()};
+                                                        clashes.push(entry)
+                                                  }
+                                              }
+                                              _ => broken_dir = true
+                                           }
+                                           
+                                       }
+                                       _ => broken_dir = true
+                                    }
+                                }
+                                _ => broken_dir = true
+                            }
+                            if n as u32 > max_cla {
+                                max_cla = n as u32
+                            }
+                        }
+                        _ => broken_dir = true
+                    }
+                    
+                }
+                _ => break
+            }
+        }
+        if broken_dir {
+            return Err(format!{"Broken the  clash directory file {file_dat:?}"})
+        }
+        max_cla += 1;
+        // search for first avail slot 
+        for avail in 0..max_cla {
+            let mut was = false;
+            for entry in &clashes {
+                if entry.num == avail {
+                    was = true;
+                    break
+                }
+            }
+            if !was { // use it
+                max_cla = avail;
+                break
+            }
+        }
+        let entry = DirEntry{num:max_cla, time: now.as_millis() as u64, key: key};
+        let num  = entry.num;
+        clashes.push(entry);
+        let _ = file.seek(SeekFrom::Start(0));
+        for entry in clashes {
+            let _ = file.write_all(format!{"{:0>5}", entry.num}.as_bytes());
+            let _ = file.write_all(format!{"{:0>14}", entry.time}.as_bytes());
+            let _ = file.write_all(&entry.key);
+        }
+        let pos = file.stream_position().map_err(|e| format!{"{e:?}"})?;
+        file.set_len(pos).map_err(|e| format!{"{e:?}"})?;
+        
+        file_dat.pop();
+        file_dat.push(format!{"{hash}-{num}"});
         file_dat.set_extension("dat");
-        let current = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+        let current = now.as_millis();
         let json = format!{r#"{{"mes": "{load}", "key" : "{random_sequence}", "active":true, "time":{current}}}"#};
         fs::write(&file_dat, json).map_err(|e| format!{"{e:?}"})?;
 
         Ok("Ok".to_owned() + &random_sequence)
     }
+}
+
+pub fn ascii_bytes_to_number(bytes: &[u8]) -> Result<i64, Box<dyn std::error::Error>> {
+    let s =  std::str::from_utf8(bytes)?;
+    let number = s.parse::<i64>()?;
+    Ok(number)
 }
--- src/rust/snap.rs
+++ src/rust/snap.rs
@@ -4,8 +4,11 @@ use std::fs;
 use std::convert::TryInto;
 use std::path::{Path,PathBuf};
 use std::time::{SystemTime,UNIX_EPOCH};
+use std::fs::OpenOptions;
+use std::io::Read;
 use data;
 use json;
+use gen;
 use json::JsonData::{Data,Text,Num};
 use KEY_LEN;
 use {DATA_DIR, FAKE_DIR};
@@ -21,10 +24,62 @@ impl simweb::WebPage for SnapPage {
         
         let key = self.key.as_bytes()[1..=KEY_LEN].try_into().unwrap();
         let hash = data::calc_hash(key);
+        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
         let mut snap_file = PathBuf::new();
+        let mut entry_num = None;
         snap_file.push(DATA_DIR);
         // TODO name will be taken from hash.col file
         snap_file.push(format!{"{hash}"});
+        snap_file.set_extension("dic");
+        // read the hash clash directory first
+        if Path::new(&snap_file).is_file() {
+            let mut file = OpenOptions::new().read(true).open(&snap_file).map_err(|e| format!{"{e:?}"})?;
+            let mut num_buf = [0; 5]; // Buffer to hold 5 bytes
+            let mut time_buf = [0; 14];
+            let mut key_buf = [0; KEY_LEN];
+            loop {
+                match file.read_exact(&mut num_buf) { // read num
+                    Ok(_) => {
+                        match gen::ascii_bytes_to_number(&num_buf) {
+                            Ok(n) => {
+                                match file.read_exact(&mut time_buf) { // read date of creation
+                                    Ok(_) => {
+                                        
+                                        match gen::ascii_bytes_to_number(&time_buf) {
+                                           Ok(time) => {
+                                               // can be skeeped if time passed
+                                               match file.read_exact(&mut key_buf) {
+                                                    Ok(_) => {
+                                                        if key == key_buf { // the match
+                                                            if now.as_millis()- (time as u128) < 1000_u128*60*60*24*7  { // 7 days
+                                                                    entry_num = Some(n)
+                                                            }
+                                                            break
+                                                        }
+                                                    }
+                                                    _ => ()
+                                               }
+                                               
+                                           }
+                                           _ => ()
+                                        }
+                                    }
+                                    _ => ()
+                                }
+                            }
+                            _ => ()
+                        }
+                    }
+                    _ => break
+                }
+            }
+        }
+        snap_file.pop();
+        if let Some(num) = entry_num {
+            snap_file.push(format!{"{hash}-{num}"})
+        } else {
+            snap_file.push(format!{"{hash}"})
+        }
         snap_file.set_extension("dat");
         if Path::new(&snap_file).is_file() {
             let json = json::parse(&fs::read_to_string(&snap_file).map_err(|e| format!{"{e:?}"})?);
@@ -33,7 +88,7 @@ impl simweb::WebPage for SnapPage {
                     if let Some(time) = ht.get("time") {
                         if let Num(time) = time {
                             let time = *time as u128;
-                            if SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()- time < 1000_u128*60*60*24*7  {
+                            if now.as_millis()- time < 1000_u128*60*60*24*7  {
                                 if let Some(text) = ht.get("mes") { 
                                     if let Text(text) = text {
                                         // delete the file
@@ -41,6 +96,8 @@ impl simweb::WebPage for SnapPage {
                                         return Ok(text.to_owned())
                                     }
                                 }
+                            } else {
+                                fs::remove_file(&snap_file).map_err(|e| format!{"{e:?}"})?;
                             }
                         } else {eprintln!{"no time"}}
                     }
@@ -49,7 +106,7 @@ impl simweb::WebPage for SnapPage {
             }
         } else { eprintln!{"no file {snap_file:?}"} }
         let mut ran = PCG32::new ();
-        let seed = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_nanos() % 100_000_000_u128;
+        let seed = now.as_nanos() % 100_000_000_u128;
         let seq = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_nanos() % 362_043_u128;
         ran.seed(seed as u64, seq as u64);
     
