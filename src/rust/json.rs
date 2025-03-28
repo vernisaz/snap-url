749bc1c4c7a0b4ffd94ddbfad1274f76c5f7bc2b
Sunil <sunil@live.com>
2025-03-28 12:26:29 -0700

    refactored range

--- src/rust/json.rs
+++ src/rust/json.rs
@@ -255,7 +255,7 @@ pub fn parse_fragment<I>(chars: &mut I ) -> (JsonData,char)
                     
                 }
             }
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+            '0' ..= '9' => {
                 match state {
                     JsonState::ObjName => {
                         field_name.push(c)
